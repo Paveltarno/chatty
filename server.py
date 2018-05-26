6f053281b15d736a6d836dba20ed7e6af9e9726c
Pavel Tarno <paveltarno@gmail.com>
2018-05-26 12:47:39 +0300

    feat(Server): Added server initilization  + shutdown

--- server.py
+++ server.py
@@ -1,25 +1,41 @@
-from tornado.options import define, options, parse_command_line
-import tornado.escape
-import tornado.ioloop
-import tornado.web
+from tornado.options import define, options
+from tornado import ioloop, web, locks
+import motor.motor_tornado
+import os
+import logging
+import sys
+import signal
+from messages.backend.messages_controller import MessagesController
 
-def main():
-    parse_command_line()
-    app = tornado.web.Application(
-        [
-            (r"/", MainHandler),
-            (r"/a/message/new", MessageNewHandler),
-            (r"/a/message/updates", MessageUpdatesHandler),
-        ],
-        cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
-        template_path=os.path.join(os.path.dirname(__file__), "templates"),
-        static_path=os.path.join(os.path.dirname(__file__), "static"),
-        xsrf_cookies=True,
-        debug=options.debug,
-    )
-    app.listen(options.port)
-    tornado.ioloop.IOLoop.current().start()
+class Server():
+    async def start(self):
+        options = self.initOptions()
+        mongoClient = motor.motor_tornado.MotorClient(options["MONGO_URL"])
+        dbConnection = mongoClient[options["DEBUG"]]
+        app = web.Application(
+            [
+                ("/messages", MessagesController),
+            ],
+            dbConnection=dbConnection, 
+            options=options,
+            debug=options["DEBUG"],
+        )
+        self.server = app.listen(options["PORT"], options["ADDRESS"])
+        logging.info("Server started on {0}:{1}".format(options["ADDRESS"] ,options["PORT"]))
+        shutdown_event = locks.Event()
+        await shutdown_event.wait()
 
+    def initOptions(self):
+        return {
+            "PORT": os.getenv("PORT", 3000),
+            "ADDRESS": os.getenv("ADDRESS", "localhost"),
+            "MAX_RESULTS": os.getenv("MAX_RESULTS", 25),
+            "MONGO_URL": os.getenv("MONGO_URL", "mongodb://localhost:27017"),
+            "DB_NAME": os.getenv("DB_NAME", "chatty"),
+            "DEBUG": os.getenv("DEBUG", "true")
+        }
 
 if __name__ == "__main__":
-    main()
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    chatServer = Server()
+    ioloop.IOLoop.current().run_sync(chatServer.start)
