fb5f121fbf7752c85525e829d081defe31f480ea
Pavel Tarno <paveltarno@gmail.com>
2018-05-26 14:09:40 +0300

    feat(MessagesController): Added get messages route

--- messages/backend/messages_controller.py
+++ messages/backend/messages_controller.py
@@ -3,33 +3,22 @@ import logging
 import os
 import dateparser
 
-MAX_RESULTS = 35
-
 class MessagesController(tornado.web.RequestHandler):
 
-  def initialize(self, dbConnection, maxConnections):
-    self.dbConnection = dbConnection
-
-  # async def get(self):
-  #   afterDate = dateparser.parse(self.get_argument("after", ""))
-  #   messages
-
-  #   if afterDate:
-  #     # Check if a `after` arguments was given, if so, return only messages after that time
-  #     # this is mainly for client polling
-  #     query = {
-  #       date: {
-  #         "$gt": afterDate
-  #       }
-  #     }
-      
-  #   else
-  #     # If no `after` date was given, return a partial history of messages
-  #     query = {
+    async def get(self):
+        afterDate = dateparser.parse(self.get_argument("after", ""))
+        messages = None
         
-  #     }
-
-
-
-
-    
+        # Check if a `after` arguments was given, if so, return only messages after that time
+        # this is mainly for client polling
+        if afterDate:
+            cursor = self.settings["dbConnection"].messages.find({"date": {
+                "$gt": afterDate
+            }}).sort([("date",-1)])
+            messages = await cursor.to_list(length=None)
+        else:
+            # If no `after` date was given, return a partial history of messages
+            cursor = self.settings["dbConnection"].messages.find({}).sort([("date",-1)])
+            messages = await cursor.to_list(length=self.settings["options"]["MAX_RESULTS"])
+        
+        self.finish({"messages": messages})
